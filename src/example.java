22ef1fd6f110dee62de1589a681d7a57fe77a763
Juergen Wurzer <wurzer.juergen@gmx.at>
2019-11-18 12:16:48 +0100

    Add java method.

--- src/example.java
+++ src/example.java
@@ -5,9 +5,15 @@ class Example
 	{
 		return a + b;
 	}
-
+	
+	public static float add2(float a, float b)
+	{
+		return a + b;
+	}
+	
 	public static void main(String [] args)
 	{
+		add2(3.4f, 3.2f);
 	}
 	
 }
